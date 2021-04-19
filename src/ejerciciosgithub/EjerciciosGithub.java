2cb8d1dcf1195423805455823025a424c9466f4f
Cecilia <Cecilia@DESKTOP-6NT86PH>
2021-04-19 12:43:45 -0600

    Este es el primer ejercicio de Github

---
+++ src/ejerciciosgithub/EjerciciosGithub.java
@@ -0,0 +1,17 @@
+/*
+ * Arturo Misael Álvarez Gutiérrez
+ * Nro. Control: 20550369
+ *
+ */
+
+package ejerciciosgithub;
+
+public class EjerciciosGithub {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo.");
+        System.out.println("Mi nombre es: Arturo Misael Alvarez Gutierrez.");
+        System.out.println("Iniciales: AMAG.");
+    }
+    
+}
