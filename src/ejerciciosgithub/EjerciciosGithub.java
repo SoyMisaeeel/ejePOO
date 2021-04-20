4619e932055693f3dec2c4839ec041a14e455ddd
Misael <>
2021-04-19 21:33:02 -0600

    Este es el primer ejercicio de Github

--- src/ejerciciosgithub/EjerciciosGithub.java
+++ src/ejerciciosgithub/EjerciciosGithub.java
@@ -6,12 +6,24 @@
 
 package ejerciciosgithub;
 
+import java.util.StringTokenizer;
+
 public class EjerciciosGithub {
 
     public static void main(String[] args) {
+        
+        String iniciales;
+        String strNombre = "Arturo Misael Alvarez Gutierrez";
+        StringTokenizer strIniciales = new StringTokenizer(strNombre);
+        
         System.out.println("Hola mundo.");
-        System.out.println("Mi nombre es: Arturo Misael Alvarez Gutierrez.");
-        System.out.println("Iniciales: AMAG.");
+        System.out.println("Mi nombre es: " + strNombre);
+        System.out.println("Mis iniciales son: " );
+        
+        while(strIniciales.hasMoreTokens()){
+            iniciales = strIniciales.nextToken();
+            System.out.println(iniciales.substring(0,1));
+        }
     }
     
 }
